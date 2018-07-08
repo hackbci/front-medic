dd8cce5d74bd7046a9a148b6caad805afb2e9fb1
Nahuel <nahuelvalentini@gmail.com>
2018-07-08 00:07:37 -0400

    building detail-history

--- src/app/shared/detail-history-patient/detail-history-patient.component.ts
+++ src/app/shared/detail-history-patient/detail-history-patient.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-history-patient.component.scss']
 })
 export class DetailHistoryPatientComponent implements OnInit {
+  panelOpenState1 = false;
+  panelOpenState2 = false;
+  panelOpenState3 = false;
 
   constructor() { }
 
